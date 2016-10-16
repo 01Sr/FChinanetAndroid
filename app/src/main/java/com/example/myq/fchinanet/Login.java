39ec8b5040a905ccf5c7758f35b42d0a7cca1938
01sr <chenbing8914@outlook.com>
2016-10-16 12:00:36 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myq/fchinanet/Login.java
@@ -0,0 +1,305 @@
+package com.example.myq.fchinanet;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Base64;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLSession;
+
+/**
+ * Created by myq on 2016/10/15.
+ */
+
+public class Login {
+    Context context;
+    String account,passwd,lastIp,author,id,serverId,brasIp,wanIp;
+    boolean isFull=false;
+    Handler handler=null;
+    public Login(Context context, String account, String passwd,Handler handler){
+        this.context=context;
+        this.account=account;
+        this.passwd=passwd;
+        this.handler=handler;
+        SharedPreferences sp=context.getSharedPreferences(context.getString(R.string.sp),1);
+        this.lastIp= sp.getString("lastIp","");
+    }
+
+
+    //检测当前设备是否已经登陆且是否设备已满
+    private boolean isLogin() throws IOException, JSONException {
+        boolean flag=false;
+        URL url=new URL("https://wifi.loocha.cn/"+id+"/wifi/status");
+        Log.e(">>>>>>>>", String.valueOf(url));
+        HttpURLConnection conn= (HttpURLConnection) url.openConnection();
+        conn.setRequestProperty("Authorization",author);
+        if(conn.getResponseCode()==200){
+            BufferedReader in=new BufferedReader(new InputStreamReader(conn.getInputStream())) ;
+            String result="";
+            String line=null;
+            while ((line = in.readLine()) != null) {
+                result += line;
+            }
+            Log.e(">>>>>>>>", result);
+            JSONObject json=new JSONObject(result);
+            if(!json.isNull("wifiOnlines")){
+                JSONArray onlines=json
+                        .getJSONObject("wifiOnlines")
+                        .getJSONArray("onlines");
+                int i=0;
+                for(;i<onlines.length();i++){
+                    JSONObject o=onlines.getJSONObject(i);
+                    if(lastIp.equals(o.getString("brasIp")))
+                        flag=true;
+                }
+                if(i==3) isFull=true;
+            }
+
+        }
+        return flag;
+    }
+    //生成author,获取id,serverId
+
+
+    //获取id,serverId
+    private String login() throws IOException, JSONException {
+        URL url=new URL("https://www.loocha.com.cn:8443/login");
+        HttpURLConnection connection=(HttpURLConnection) url.openConnection();
+        connection.setRequestProperty("Authorization", author);
+        Log.e(">>>>>>>>>>",author);
+        switch(connection.getResponseCode()){
+            case 200:
+                BufferedReader in=new BufferedReader(new InputStreamReader(connection.getInputStream())) ;
+                String result="";
+                String line=null;
+                while ((line = in.readLine()) != null) {
+                    result += line;
+                }
+                JSONObject json=new JSONObject(result);
+                int status=json.getInt("status");
+                if(status==0){
+                    id=json.getJSONObject("user").getString("id");
+                    serverId=json.getJSONObject("user").getString("did").replaceFirst("#.*", "");
+                    return "0";
+                }else{
+                    return "login异常";
+                }
+            case 401:
+                return"账号或密码错误";
+        }
+        return "login异常";
+    }
+    private String getQRCode() throws IOException, JSONException {
+        URL url=new URL("https://wifi.loocha.cn/0/wifi/qrcode" + "?brasip="+brasIp + "&ulanip=" +wanIp + "&wlanip=" +wanIp);
+        HttpURLConnection connection=(HttpURLConnection) url.openConnection();
+        BufferedReader in=new BufferedReader(new InputStreamReader(connection.getInputStream())) ;
+        String result="";
+        String line=null;
+        while ((line = in.readLine()) != null) {
+            result += line;
+        }
+
+        JSONObject json=new JSONObject(result);
+
+        int status=json.getInt("status");
+        if(status==0){
+
+            String qrcode= json.getJSONObject("telecomWifiRes").getString("password");
+
+            return qrcode;
+        }else{
+            return "";
+        }
+    }
+    private String getPasswd() throws IOException, JSONException {
+        URL url=new URL("https://www.loocha.com.cn/"+id+"/wifi?server_did="+serverId);
+        HttpURLConnection connection=(HttpURLConnection) url.openConnection();
+        connection.setRequestProperty("Authorization", author);
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        if(connection.getResponseCode()==200){
+            String result="";
+            String line=null;
+            while ((line = in.readLine()) != null) {
+                result += line;
+            }
+            Log.e(">>>>>>>>",result);
+            JSONObject json=new JSONObject(result);
+            JSONObject telecomWifiRes=json.getJSONObject("telecomWifiRes");
+            if(telecomWifiRes.isNull("password")) return "获取密码失败，请在掌上大学重新获取密码后尝试";
+            return  telecomWifiRes.getString("password");
+        }
+    return "密码获取失败";
+
+    }
+    private String getIp() throws IOException {
+        URL url=new URL("http://test.f-young.cn");
+        HttpURLConnection conn=(HttpURLConnection) url.openConnection();
+        conn.setInstanceFollowRedirects(false);
+        if(conn.getResponseCode()==302){
+            String param=conn.getHeaderField("Location").split("\\?")[1];
+            String []args=param.split("&");
+            wanIp=args[0].split("=")[1];
+            brasIp=args[1].split("=")[1];
+            return "0";
+        }
+        return "未能获取设备ip,请检查是否连接至校园网，或已登录";
+    }
+
+    private String dialUp(String qrCode,String code) throws IOException, JSONException {
+        URL realUrl = new URL("https://wifi.loocha.cn/"+id+"/wifi/enable?qrcode="+qrCode+ "&code="+code);
+        HttpsURLConnection conn = (HttpsURLConnection) realUrl.openConnection();
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Authorization", author);
+        conn.setRequestProperty("connection", "Keep-Alive");
+        conn.setRequestProperty("Accept-Language", "zh-CN,zh;q=0.8,en;q=0.6");
+        conn.setDoOutput(true);
+        conn.setDoInput(true);
+        if(conn.getResponseCode()==200){
+            BufferedReader in = null;
+            String result = "";
+            in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line;
+            while ((line = in.readLine()) != null) {
+                result += line;
+            }
+            JSONObject json=new JSONObject(result);
+            int status=json.getInt("status");
+            if(status==0){
+                SharedPreferences sp=context.getSharedPreferences(context.getString(R.string.sp),1);
+                SharedPreferences.Editor ed=sp.edit();
+                ed.putString("lastIp",wanIp);
+                ed.putString("brasIp",brasIp);
+                ed.commit();
+                this.lastIp= sp.getString("lastIp","");
+                return "登陆成功";
+            }
+            else{
+                return json.getString("response");
+            }
+        }
+        return "登录失败";
+
+    }
+
+    private String kickOff() throws IOException {
+        SharedPreferences sp=context.getSharedPreferences(context.getString(R.string.sp),1);
+        wanIp=sp.getString("lastIp","");
+        brasIp=sp.getString("brasIp","");
+        if(wanIp.equals("")||brasIp.equals("")) return "尚未登陆";
+        URL url=new URL("https://wifi.loocha.cn/"+id+"/wifi/kickoff?wanip="+wanIp+"&brasip="+brasIp);
+        HttpURLConnection conn=(HttpURLConnection) url.openConnection();
+        conn.setRequestProperty("Authorization", author);
+        Log.e(">>>>>>>>>",author);
+        conn.setRequestMethod("DELETE");
+        int code=conn.getResponseCode();
+        if(code==200){
+            return "下线成功";
+        }
+        return "下线失败";
+    }
+
+    public void online(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                javax.net.ssl.HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+
+                    @Override
+                    public boolean verify(String hostname, SSLSession session) {
+                        // TODO Auto-generated method stub
+                        return true;
+                    }
+                });
+                String info="";
+                String ap=account+":"+passwd;
+                author=Base64.encodeToString(ap.getBytes(),Base64.DEFAULT);
+                author="Basic "+author;
+                try {
+                    info=login();
+                    Log.e(">>>>>>>>>",info);
+                    if(!info.equals("0"))return;
+                    if(isLogin()){info="已经登陆";return;}
+                    Log.e(">>>>>>>>>",info);
+                    if(isFull){ info="设备已满";return;}
+                    info=getIp();
+                    Log.e(">>>>>>>>>",info);
+                    if(!info.equals("0"))return;
+//                    for(int i=0;i<3;i++){
+                        String qrCode=getQRCode();
+                        Log.e(">>>>>>>>>",qrCode);
+                        if(qrCode.equals("")){ info="code获取异常";return;}
+                        info=getPasswd();
+                        Log.e(">>>>>>>>>",info);
+                        if(!info.matches("\\d{6}")) return;
+                        String code=info;
+                        info=dialUp(qrCode,code);
+//                    }
+                } catch (IOException e) {
+                    info="网络访问异常";
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    info="数据解析异常";
+                    e.printStackTrace();
+                }finally {
+                    if(handler!=null){
+                        Message m=new Message();
+                        m.what=1;
+                        m.obj=info;
+                        handler.sendMessage(m);
+                    }
+
+                }
+            }
+        }).start();
+
+
+    }
+
+    public void offline(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String info = "";
+                String ap=account+":"+passwd;
+                Log.e(">>>>>>>>>>",ap);
+                author=Base64.encodeToString(ap.getBytes(),Base64.DEFAULT);
+                author="Basic "+author;
+                try {
+                    info=login();
+                    if(!info.equals("0"))return;
+                    info=kickOff();
+                } catch (IOException e) {
+                    info="网络访问异常";
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    info="数据解析异常";
+                    e.printStackTrace();
+                }finally {
+                    if(handler!=null){
+                        Message m=new Message();
+                        m.what=1;
+                        m.obj=info;
+                        handler.sendMessage(m);
+                    }
+                }
+            }
+        }).start();
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/myq/fchinanet/LoginActivity.java
@@ -0,0 +1,208 @@
+package com.example.myq.fchinanet;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.content.SharedPreferences;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.app.LoaderManager.LoaderCallbacks;
+
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.Manifest.permission.READ_CONTACTS;
+
+/**
+ * A login screen that offers login via account/password.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    /**
+     * Id to identity READ_CONTACTS permission request.
+     */
+    private static final int REQUEST_READ_CONTACTS = 0;
+
+    /**
+     * A dummy authentication store containing known user names and passwords.
+     * TODO: remove after connecting to a real authentication system.
+     */
+    private static final String[] DUMMY_CREDENTIALS = new String[]{
+            "foo@example.com:hello", "bar@example.com:world"
+    };
+    /**
+     * Keep track of the login task to ensure we can cancel it if requested.
+     */
+
+    // UI references.
+    private EditText mAccountView;
+    private EditText mPasswordView;
+    private View mProgressView;
+    private View mLoginFormView;
+    private MyHandler myHandler=null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        // Set up the login form.
+        mAccountView = (EditText) findViewById(R.id.account);
+
+        mPasswordView = (EditText) findViewById(R.id.password);
+
+        Button signin = (Button) findViewById(R.id.signin_btn);
+        signin.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attempt(true);
+            }
+        });
+        Button logout= (Button) findViewById(R.id.logout_btn);
+        logout.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attempt(false);
+            }
+        });
+        mLoginFormView = findViewById(R.id.login_form);
+        mProgressView = findViewById(R.id.login_progress);
+
+        SharedPreferences sp=getSharedPreferences(getString(R.string.sp),1);
+        if(sp.contains("account"))
+            mAccountView.setText(sp.getString("account",""));
+        if(sp.contains("passwd"))
+            mPasswordView.setText(sp.getString("passwd",""));
+    }
+
+    class MyHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            if(msg.what==1){
+
+                showProgress(false);
+                String info= String.valueOf(msg.obj);
+                Toast.makeText(LoginActivity.this,String.valueOf(msg.obj),Toast.LENGTH_SHORT).show();
+
+            }
+        }
+    }
+
+
+
+    /**
+     * Attempts to sign in or register the account specified by the login form.
+     * If there are form errors (invalid account, missing fields, etc.), the
+     * errors are presented and no actual login attempt is made.
+     */
+    private void attempt(boolean l) {
+        SharedPreferences sp=getSharedPreferences(getString(R.string.sp),1);
+        SharedPreferences.Editor edit=sp.edit();
+        edit.putString("account", String.valueOf(mAccountView.getText()));
+        edit.putString("passwd", String.valueOf(mPasswordView.getText()));
+        edit.commit();
+        if (myHandler == null) myHandler=new MyHandler();
+
+        // Reset errors.
+        mAccountView.setError(null);
+        mPasswordView.setError(null);
+
+        // Store values at the time of the login attempt.
+        String account = mAccountView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        // Check for a valid password, if the user entered one.
+        if (TextUtils.isEmpty(password)) {
+            mPasswordView.setError(getString(R.string.error_field_required));
+            focusView = mPasswordView;
+            cancel = true;
+        }
+
+        // Check for a valid account address.
+        if (TextUtils.isEmpty(account)) {
+            mAccountView.setError(getString(R.string.error_field_required));
+            focusView = mAccountView;
+            cancel = true;
+        }
+
+        if (cancel) {
+            focusView.requestFocus();
+        } else {
+            showProgress(true);
+            Login login=new Login(this,account,password,myHandler);
+            if(l) login.online();
+            else login.offline();
+        }
+    }
+
+
+
+    /**
+     * Shows the progress UI and hides the login form.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            mLoginFormView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
+    }
+
+
+
+
+
+}
+
